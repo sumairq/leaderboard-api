e60a3610e12f166b025aa9fb37a89105cd002e6f
Sumair <sumairqaisar992@gmail.com>
2022-02-09 15:25:22 +0500

    Fix linter erros

--- src/index.js
+++ src/index.js
@@ -1,50 +1,40 @@
-import _ from 'lodash';
 import './style.css';
 
-const scoreList = document.querySelector('.scores-body ul')
-
+const scoreList = document.querySelector('.scores-body ul');
 
 const scores = [
   {
     name: 'Sumair',
-    score: 100
+    score: 100,
   },
   {
     name: 'John',
-    score: 200
+    score: 200,
   },
   {
     name: 'Allen',
-    score: 123
+    score: 123,
   },
   {
     name: 'Alice',
-    score: 140
+    score: 140,
   },
   {
     name: 'Rodger',
-    score: 65
-  }
-]
+    score: 65,
+  },
+];
 
-
-function populateScores(){
-scores.forEach((item)=>{
-
-  const newItem = document.createElement('li')
-  newItem.innerHTML = ` <li>
+function populateScores() {
+  scores.forEach((item) => {
+    const newItem = document.createElement('li');
+    newItem.innerHTML = ` <li>
   <p>${item.name}: ${item.score}</p>
-</li>`
-console.log(item.name)
-scoreList.appendChild(newItem)
-
-})
-
-
+</li>`;
+    scoreList.appendChild(newItem);
+  });
 }
 
-
-window.addEventListener('DOMContentLoaded', (event) => {
-  console.log('DOM fully loaded and parsed');
-  populateScores()
+window.addEventListener('DOMContentLoaded', () => {
+  populateScores();
 });
