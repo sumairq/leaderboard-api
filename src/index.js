09ff64a384381a3e31f5abb3aae6f72d013b9431
Sumair <sumairqaisar992@gmail.com>
2022-02-09 14:41:00 +0500

    Add basic markup setup for leaderboard

--- src/index.js
+++ src/index.js
@@ -1,14 +1,50 @@
 import _ from 'lodash';
 import './style.css';
 
-function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-  
-    return element;
+const scoreList = document.querySelector('.scores-body ul')
+
+
+const scores = [
+  {
+    name: 'Sumair',
+    score: 100
+  },
+  {
+    name: 'John',
+    score: 200
+  },
+  {
+    name: 'Allen',
+    score: 123
+  },
+  {
+    name: 'Alice',
+    score: 140
+  },
+  {
+    name: 'Rodger',
+    score: 65
   }
-  
-  document.body.appendChild(component());
+]
+
+
+function populateScores(){
+scores.forEach((item)=>{
+
+  const newItem = document.createElement('li')
+  newItem.innerHTML = ` <li>
+  <p>${item.name}: ${item.score}</p>
+</li>`
+console.log(item.name)
+scoreList.appendChild(newItem)
+
+})
+
+
+}
+
+
+window.addEventListener('DOMContentLoaded', (event) => {
+  console.log('DOM fully loaded and parsed');
+  populateScores()
+});
